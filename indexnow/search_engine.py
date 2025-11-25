798cbb96288415f6f5731bbd622fb5711c215af1
Tomáš Cejpek <tomas.cejpek@email.cz>
2025-11-25 19:53:10 +0100

    add more endpoints

--- indexnow/search_engine.py
+++ indexnow/search_engine.py
@@ -3,3 +3,9 @@ from enum import Enum
 
 class SearchEngine(Enum):
     INDEX_NOW = 'https://api.indexnow.org/indexnow'
+    SEZNAM_CZ = 'https://search.seznam.cz/indexnow'
+    AMAZON = 'https://indexnow.amazonbot.amazon/indexnow'
+    BING = 'https://www.bing.com/indexnow'
+    YANDEX = 'https://yandex.com/indexnow'
+    NAVER = 'https://searchadvisor.naver.com/indexnow'
+    YEP = 'https://indexnow.yep.com/indexnow'
